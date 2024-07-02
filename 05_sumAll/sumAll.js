de39e02de5b6996e8d3edc92420772a6ea5af940
Dylan Atianzar <69043855+dylanatianzar@users.noreply.github.com>
2024-07-02 11:35:59 -0700

    Solve 01-07 Javascript Excersises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if ((firstNum < 0 || secondNum < 0) || (!Number.isInteger(firstNum) || !Number.isInteger(secondNum))) {
+        return "ERROR";
+    }
+    let total = 0;
+    let first = firstNum, second = secondNum;
 
+    if (firstNum > secondNum) {
+        first = secondNum;
+        second = firstNum;
+    }
+
+    for (let i = first; i <= second; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let converted = ((degrees-32)*(5/9));
+  return (converted % 1 === 0) ? converted : Number(converted.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let converted = ((degrees*(9/5))+32);
+  return (converted % 1 === 0) ? converted : Number(converted.toFixed(1));
 };
 
 // Do not edit below this line
